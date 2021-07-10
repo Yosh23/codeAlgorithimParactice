342e79cb3f65c41f102fdd5071588a1a38af6e67
Josh <josh@Joshs-MacBook-Pro.local>
2021-07-10 17:23:41 -0600

    comment update

--- twoSum.js
+++ twoSum.js
@@ -1,25 +1,28 @@
 let nums1 = [11,7,2,15]
 let nums1Answer = 9
 
-let nums2 = [3,2,4]
-let nums2Answer = 6
 
-let nums3 = [3,3]
-let nums3Answer = 6
 
-const sumTwo = (x,y) => {
+// this is NOT good and requires to much brute force doesn't account for if array index of 2 and 0 equals 9 as well and so forth. not going to work every time.
+const sumTwoIf = (x,y) => {
     let result
     if (x[0] + x[1] == y) {
         result = `${x[0]} + ${x[1]} = ${y}`
     } else if (x[1] + x[2] == y) {
         result = `${x[1]}[1] + ${x[2]}[2] = ${y}`
-        
+
     } else if (x[2] + x[3] == y) {
         result = `${x[2]} + ${x[3]} = ${y}`
     } else {
-        result = "incorrect"
+        result = "Solution not found"
     }
     return result;
 }
 
-console.log (sumTwo(nums1,nums1Answer));
+console.log (sumTwoIf(nums1,nums1Answer));
+
+
+
+
+
+
