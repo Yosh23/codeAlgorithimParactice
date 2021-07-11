297a0f67a9063fc03d32f1de001a451579938696
Josh <josh@Joshs-MacBook-Pro.local>
2021-07-10 23:27:39 -0600

    parseInt duh

--- reverseInt.js
+++ reverseInt.js
@@ -6,22 +6,25 @@
 //my though process
 let myNum = -3120
 let numStr = myNum.toString()
+console.log(numStr)
 let numArray = numStr.split('')
+console.log(numArray)
 let arrayReverse = numArray.reverse()
+console.log(arrayReverse)
 let arrayJoin = arrayReverse.join('')
-let testParse = parseFloat(arrayJoin)
-
 console.log(arrayJoin)
+let parse = parseInt(arrayJoin)
+console.log(parse)
 
 
 
 // moving thought process to function
 
-const reverse = x =>  x.toString().split('').reverse().join('')
+const reverse = x =>  parseInt(x.toString().split('').reverse().join(''))
 
 console.log(reverse(myNum))
 
-// how do I deal with the negative? and remove 0's when reversed? is there a funtion I'm not aware of that does this?
+// how do I deal with the negative? and remove 0's when reversed? is there a funtion I'm not aware of that does this? 
 // Do I for loop and check does index 0 have a 0 if so remove?
 // If I do the for loop how to I keep it from removing 0s from the middle of the number that need to be included?
 
