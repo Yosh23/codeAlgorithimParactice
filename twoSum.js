d9d71671a131873a5c48b7c9ae9211a525a6aedb
Josh <josh@Joshs-MacBook-Pro.local>
2021-07-10 18:06:06 -0600

    added code question

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,7 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
 let nums1 = [11,7,2,15]
 let nums1Answer = 9
 
@@ -9,7 +13,7 @@ const sumTwoIf = (x,y) => {
     if (x[0] + x[1] == y) {
         result = `${x[0]} + ${x[1]} = ${y}`
     } else if (x[1] + x[2] == y) {
-        result = `${x[1]}[1] + ${x[2]}[2] = ${y}`
+        result = `${x[1]} (array index [1]) + ${x[2]} (array index [2]) = ${y}`
 
     } else if (x[2] + x[3] == y) {
         result = `${x[2]} + ${x[3]} = ${y}`
@@ -21,6 +25,7 @@ const sumTwoIf = (x,y) => {
 
 console.log (sumTwoIf(nums1,nums1Answer));
 
+// is a for loop a better option to iterate through the array?. or would .reduce() be better? probably not reduce.
 
 
 
