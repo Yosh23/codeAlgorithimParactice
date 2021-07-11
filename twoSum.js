fa388ff0ceb0e66d2fe6376fb51437a3c22025b8
Josh <josh@Joshs-MacBook-Pro.local>
2021-07-10 20:42:14 -0600

    added one more test array

--- twoSum.js
+++ twoSum.js
@@ -9,6 +9,8 @@ let nums2 = [1,9,8]
 let nums2Answer = 10
 let nums3 = [1,2,3,4,5,6]
 let nums3Answer = 10
+let nums4 = [1,2,3,4,5,6,7,8,9]
+let nums4Answer = 17
 
 
 // this is NOT good and requires to much brute force doesn't account for if array index of 2 and 0 equals 9 as well and so forth. not going to work every time.
@@ -48,5 +50,5 @@ const twoSumFor = (nums, target) => {
 console.log(twoSumFor(nums1,nums1Answer))
 console.log(twoSumFor(nums2,nums2Answer))
 console.log(twoSumFor(nums3,nums3Answer))
-
+console.log(twoSumFor(nums4,nums4Answer))
 // can I substract the Target/Answer from a number and see if that number is in the array? 
